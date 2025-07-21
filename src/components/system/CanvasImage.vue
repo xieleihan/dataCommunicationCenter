b31afbcf04efe2d0692ed358419cc7c9052134d5
xieleihan <xieleihan@gmail.com>
2025-07-21 19:16:16 +0800

    feat

--- src/components/system/CanvasImage.vue
+++ src/components/system/CanvasImage.vue
@@ -2,27 +2,107 @@
     <div class="canvasImage">
         <div class="left">
             <el-upload
+                ref="uploadRef"
                 :action="base_url"
                 list-type="picture-card"
                 :on-success="handleSuccess"
                 :on-preview="handlePictureCardPreview"
                 :on-remove="handleRemove"
+                :on-exceed="handleExceed"
+                :limit="1"
+                :before-upload="beforeUpload"
             >
                 <el-icon>
                     <Plus />
                 </el-icon>
             </el-upload>
         </div>
-        <div class="right"></div>
+        <div class="right">
+            
+        </div>
     </div>
+
+    <!-- 图片预览对话框 -->
+    <el-dialog
+        v-model="dialogVisible"
+        title="图片预览"
+    >
+        <img
+            :src="dialogImageUrl"
+            style="width: 100%"
+        />
+    </el-dialog>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref } from 'vue'
+import { ElMessage, ElDialog } from 'element-plus'
 
+const isLocalhost = window.location.hostname === 'localhost'
 const base_url = ref(isLocalhost
     ? import.meta.env.VITE_BASE_API + '/upload'
     : import.meta.env.VITE_BASE_LAN_API + '/upload')
+
+const dialogImageUrl = ref('')
+const dialogVisible = ref(false)
+const uploadRef = ref(null)
+
+const pic = ref(null)
+
+const beforeUpload = (file) => {
+    const isValid = file.type.startsWith('image/')
+    if (!isValid) {
+        ElMessage.error('只能上传图片文件')
+    }
+    return isValid
+}
+
+const handleSuccess = (uploadFile) => {
+    console.log('上传成功:', uploadFile)
+    if (uploadFile.code === 200) {
+        const obj = {
+            url: uploadFile.data.url,
+            name: uploadFile.data.alt,
+            uid: Date.now() + Math.random().toString(36).substr(2, 9),
+        }
+        pic.value = obj
+    } else {
+        ElMessage.error('上传失败')
+    }
+}
+
+const handlePictureCardPreview = (uploadFile) => {
+    dialogImageUrl.value = uploadFile.url
+    dialogVisible.value = true
+}
+
+const handleRemove = (uploadFile) => {
+    const filename = uploadFile.response?.data?.alt
+    if (!filename) {
+        ElMessage.error('文件信息异常')
+        return
+    }
+
+    deleteUploadImage({ filename })
+        .then((res) => {
+            if (res.code === 200) {
+                ElMessage.success('删除成功')
+            } else {
+                ElMessage.error('删除失败')
+            }
+        })
+        .catch((err) => {
+            console.error('删除失败:', err)
+            ElMessage.error('删除失败')
+        })
+
+    pic.value = null
+}
+
+const handleExceed = () => {
+    ElMessage.warning('只能上传一张图片')
+    uploadRef.value?.clearFiles()
+}
 </script>
 
 <style lang="scss" scoped>
