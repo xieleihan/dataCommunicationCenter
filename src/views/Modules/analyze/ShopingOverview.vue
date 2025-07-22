e6e158c1f2f852cc7c6b0dcf2e796f0cf9097333
xieleihan <xieleihan@gmail.com>
2025-07-22 11:42:24 +0800

    feat

--- src/views/Modules/analyze/ShopingOverview.vue
+++ src/views/Modules/analyze/ShopingOverview.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="shopingEcharacter">
         销售情况(可视化)
     </div>
 </template>
@@ -9,5 +9,11 @@
 </script>
 
 <style lang="scss" scoped>
-
+.shopingEcharacter {
+    width: 100%;
+    height: 100%;
+    padding: .1rem;
+    overflow: auto;
+    
+}
 </style>
